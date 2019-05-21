ea957c93a096506cd30b61cc14da2b2e5b4bffda
Jonathan Bruno <jonathanbruno.ueg@gmail.com>
2019-05-21 17:49:34 -0300

    add oh my zsh

--- install.sh
+++ install.sh
@@ -1,56 +1,60 @@
 SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 echo $SCRIPTPATH
 
-echo -e "\e[92mInstalling zsh\e[39m"
+echo -e "\e[92m Installing zsh\e[39m"
 sudo apt-get -y install zsh.
 
-echo -e '\e[92mDefining zsh as default shell (You need to re-login to make it work)\e[39m'
+echo -e '\e[92m Defining zsh as default shell (You need to re-login to make it work)\e[39m'
 chsh -s $(which zsh)
 
-echo -e '\e[Copying .zshrc file\e[39m'
-cp $SCRIPTPATH/zshrc ~/.zshrc
+#echo -e '\e[Copying .zshrc file\e[39m'
+#cp $SCRIPTPATH/zshrc ~/.zshrc
 
 FZF_REPO_DIR=fzf
 if [ -d "$FZF_REPO_DIR" ]; then
-    echo -e '\e[92mDeleting fzf repo directory\e[39m'
+    echo -e '\e[92m Deleting fzf repo directory\e[39m'
     sudo rm -r $FZF_REPO_DIR
 fi
 
 FZF_DIR=~/.fzf
 if [ -d "$FZF_DIR" ]; then
-    echo -e '\e[92mDeleting fzf directory\e[39m'
+    echo -e '\e[92m Deleting fzf directory\e[39m'
     sudo rm -r $FZF_DIR
 fi
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-echo -e '\e[92mInstalling fzf\e[39m'
+echo -e '\e[92m Installing fzf\e[39m'
 ~/.fzf/install --all
 
-echo -e '\e[92mInstalling Vim-Plug\e[39m'
+echo -e '\e[92m Installing Vim-Plug\e[39m'
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-VIM_COLOR_DIR=~/.vim/colors
-if ![ -d "$VIM_COLOR_DIR" ]; then
-    echo -e '\e[92mcreating vim color directory\e[39m'
-    sudo rm -r $VIM_COLOR_DIR
-fi
+VIM_COLOR_DIR=~/.vim/colors/
+echo -e '\e[92m Creating vim color directory\e[39m'
+sudo mkdir $VIM_COLOR_DIR
 
 DRACULA_REPO_DIR=/tmp/vim
 if [ -d "$DRACULA_REPO_DIR" ]; then
-    echo -e '\e[92mDeleting dracula repo directory\e[39m'
+    echo -e '\e[92m Deleting dracula repo directory\e[39m'
     sudo rm -r $DRACULA_REPO_DIR
 fi
 
 git clone https://github.com/dracula/vim.git $DRACULA_REPO_DIR
 
-echo -e '\e[Copying dracula theme\e[39m'
-cp $DRACULA_REPO_DIR/colors/*.vim $VIM_COLOR_DIR
+echo -e '\e[92m Copying dracula theme\e[39m'
+sudo cp $DRACULA_REPO_DIR/colors/*.vim $VIM_COLOR_DIR
 
-echo -e '\e[Copying .vimrc file\e[39m'
+echo -e '\e[92m Copying .vimrc file\e[39m'
 cp $SCRIPTPATH/vimrc ~/.vimrc
 
-echo -e '\e[Installing Vim plugins\e[39m'
+echo -e '\e[92m Installing Vim plugins\e[39m'
 vim +PlugInstall +qall
+
+echo -e '\e[92m Installing oh My zsh \e[39m'
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+sudo cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+source ~/.zshrc
+
